d725b3f7cc6568ea7a4e74fdbad2426b32f2ec80
emfpdlzj <redryan@ajou.ac.kr>
2025-12-03 00:52:42 +0900

    feat: 클릭시 빌딩 선택창 이동 기능 구현 및 더 디자인 수정

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useNavigate, useLocation } from 'react-router-dom' // 라우팅 관련
 import { useAuth } from '../hooks/useAuth'
-import parkingicon from '../assets/icons/parking.svg'
+import parkingicon from '../assets/icons/parking_black.png'
 
 export default function Header() {
     const { accessToken, member, logout } = useAuth()
@@ -19,14 +19,17 @@ export default function Header() {
     return (
         <header className="w-full flex items-center justify-between px-10 py-4 bg-white shadow-sm">
             <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 cursor-pointer hover:bg-slate-100 px-2 py-1 rounded transition"
+                    onClick={()=>navigate('/BuildingSelectPage')}>
                 <img
                     src={parkingicon}
                     alt="주차 아이콘"
                     className="w-6 h-6 object-contain"
                 />
                 <span className="text-sm font-semibold text-slate-800">
-                    AJOU UNIV. 주차 관리 시스템
+                    AJOU UNIV. 주차 관리 시스템  |  A Parking
                 </span>
+                </div>
             </div>
 
             <div className="flex items-center gap-4">
@@ -46,7 +49,7 @@ export default function Header() {
                 ) : (
                     <Link
                         to="/login"
-                        className="px-4 py-1.5 text-sm font-medium rounded-full border border-[#0b57d0] text-[#0b57d0] bg-white hover:bg-[#e5efff] transition"
+                        className="px-4 py-1.5 text-sm font-medium border border-[#0b57d0] text-white bg-[#0b57d0] hover:bg-[#174ea6] rounded-lg transition"
                     >
                         로그인
                     </Link>
