041dd9f16665e92994823b2c3c3f6fb9f1e70922
wonbin <wonbin3977@gmail.com>
2025-12-04 00:15:05 +0900

    fix : vercel과 연결중

--- express/server.js
+++ express/server.js
@@ -7,11 +7,6 @@ import './service/redis/redisSubscriber.js';
 import initialize from './controller/caching/initialize.js';
 import ParkingStatus from './controller/building/ParkingFee.js';
 import { buildingSummaryRouter } from './controller/summary/BuildingSummary.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -19,7 +14,6 @@ const app = express();
 dotenv.config();
 applyCors(app); //cors 설정 적용
 app.use(express.json());  // JSON body 파싱
-app.use(express.static(path.join(__dirname, "../react/dist")));
 
 app.use(authRoutes); // 인증 라우트
 app.use(buildingSelector);
@@ -29,9 +23,6 @@ app.use(buildingSummaryRouter);
 
 app.get('/', (req, res) => {
   res.send('8080 서버 응답');
-});
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../react/dist/index.html"));
 });
 
 app.listen(8080, () => {
