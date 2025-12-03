dd245c95e5896de09245a0ed0c4b1c3daeb83047
wonbin <wonbin3977@gmail.com>
2025-12-03 15:43:48 +0900

    fix(cors) : 웹소켓 연결 오류 수정

--- express/configuration/corsConfig.js
+++ express/configuration/corsConfig.js
@@ -4,7 +4,6 @@ export const corsOptions = {
   origin: [
     "http://localhost:5173",       
     "https://a-parking.vercel.app",
-    "https://a-parking-4fufdqnmz-wonbin01s-projects.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
